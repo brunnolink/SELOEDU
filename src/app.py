a56987ca2f5bcfdbcd3261d94a390a746b8ebbf9
brunno <brunnolinker@gmail.com>
2025-09-18 15:24:10 -0300

    feat: -new page home, new forms login and new activitie done

--- src/app.py
+++ src/app.py
@@ -1,24 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
- 
-users = [
-    {"id": 1, "name": "Jose Mauricio", "email": "joseMauricio@email.com", "perfil": "Professor", "status": "Ativo"},
-    {"id": 2, "name": "Brunno", "email": "brunno@email.com", "perfil": "Estudante", "status": "Inativo"},
-    {"id": 3, "name": "Carla", "email": "carla@email.com", "perfil": "Professor", "status": "Ativo"},
-    {"id": 4, "name": "Daniel", "email": "daniel@email.com", "perfil": "Estudante", "status": "Ativo"},
-    {"id": 5, "name": "Eduarda", "email": "eduarda@email.com", "perfil": "Admin", "status": "Inativo"},
-    {"id": 6, "name": "Felipe", "email": "felipe@email.com", "perfil": "Estudante", "status": "Ativo"},
-    {"id": 7, "name": "Gabriela", "email": "gabriela@email.com", "perfil": "Professor", "status": "Ativo"},
-]
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def home():
+    return render_template("home.html")
 
-@app.route("/users")
-def listar_usuarios():
-    return render_template("users.html", users=users)
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        
+        return redirect(url_for("home"))   
+    return render_template("auth/login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
